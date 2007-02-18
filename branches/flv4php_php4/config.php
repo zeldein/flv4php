13e8f0e77a8e7df28cde094a11b99dd21926234f
fannoj <fannoj@764f2fee-221d-0410-aed0-b5b95a976e14>
2007-02-18 03:02:14 +0000

    added: Support for codec 5 ( vp6 with alpha levels ) added: IP Blok for lock playback.

--- branches/flv4php_php4/config.php
+++ branches/flv4php_php4/config.php
@@ -3,5 +3,6 @@ $config['db_host'] = "localhost";
 $config['db_username'] = "username";
 $config['db_password'] = "************";
 $config['db_name'] = "Database_name";
-$config['db_table'] = "table_name";
+$config['db_table_prefix'] = "flv_";
+$config['block_counter'] = 0;
 ?>
